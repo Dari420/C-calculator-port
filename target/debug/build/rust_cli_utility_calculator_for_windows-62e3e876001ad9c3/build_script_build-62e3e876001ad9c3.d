3b788d9416cc34039929f934ae718095af5aa4e4
Dari <Dari@dari.com>
2020-03-03 19:40:25 -0500

    updated cargo.toml and binaries

---
+++ target/debug/build/rust_cli_utility_calculator_for_windows-62e3e876001ad9c3/build_script_build-62e3e876001ad9c3.d
@@ -0,0 +1,5 @@
+C:\Users\alexc\Documents\GitHub\C-calculator-port-Rust\target\debug\build\rust_cli_utility_calculator_for_windows-62e3e876001ad9c3\build_script_build-62e3e876001ad9c3.exe: build.rs
+
+C:\Users\alexc\Documents\GitHub\C-calculator-port-Rust\target\debug\build\rust_cli_utility_calculator_for_windows-62e3e876001ad9c3\build_script_build-62e3e876001ad9c3.d: build.rs
+
+build.rs:
---
+++ target/release/build/rust_cli_utility_calculator_for_windows-dd8476e023ddd6ae/build_script_build-dd8476e023ddd6ae.d
@@ -0,0 +1,5 @@
+C:\Users\alexc\Documents\GitHub\C-calculator-port-Rust\target\release\build\rust_cli_utility_calculator_for_windows-dd8476e023ddd6ae\build_script_build-dd8476e023ddd6ae.exe: build.rs
+
+C:\Users\alexc\Documents\GitHub\C-calculator-port-Rust\target\release\build\rust_cli_utility_calculator_for_windows-dd8476e023ddd6ae\build_script_build-dd8476e023ddd6ae.d: build.rs
+
+build.rs:
